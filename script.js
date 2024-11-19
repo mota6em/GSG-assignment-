c1529c53407aa22c0aec4e8ab73ecc0cd51ab0c8
𝙰𝚋𝚞𝚋𝚊𝚛𝚊𝚔𝚊 𝙼𝚘𝚝𝚊𝚜𝚎𝚖 <146123311+mota6em@users.noreply.github.com>
2024-11-19 10:39:43 +0100

    JavaScript Fundementals: Task 3

--- script.js
+++ script.js
@@ -4,5 +4,18 @@ const numArray = [2, 34, 21, 112, 53, 23, 12];
 let average =
   numArray.reduce((acc, currentValue) => acc + currentValue, 0) /
   numArray.length;
-
 console.log("The average of the number array is: ", average.toFixed(2));
+
+//Task 3
+const stringArray = [
+  "Ahmed",
+  "Mohammed",
+  "Rami",
+  "Ahmed", //duplicated
+  "Rami", //duplicated
+  "Motasem",
+  "Mohammed", //duplicated
+];
+
+let uniqueStringArray = [...new Set(stringArray)];
+console.log("Unique Values of the String Array: ", uniqueStringArray);
