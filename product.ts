c1e1896f3d1e7bda02d25ba1b263e1845cd8a1cd
𝙰𝚋𝚞𝚋𝚊𝚛𝚊𝚔𝚊 𝙼𝚘𝚝𝚊𝚜𝚎𝚖 <146123311+mota6em@users.noreply.github.com>
2024-11-19 11:15:56 +0100

    TypeScript Basics: Task 5

--- product.ts
+++ product.ts
@@ -3,8 +3,14 @@ interface Product {
   name: string;
   price: number;
 }
-function getTotalPrice(params: Product[]) : number {
+function getTotalPrice(params: Product[]): number {
   let totalPrice = 0;
   params.forEach((product) => (totalPrice += product.price));
   return totalPrice;
+}
+
+//Task 5
+const emailRegex = new RegExp("^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$");
+function isValidEmail(email: string): boolean {
+  return emailRegex.test(email);
 }
